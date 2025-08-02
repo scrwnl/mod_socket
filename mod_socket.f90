f9447eb4a50d1652acde221f626b1b9d76064d76
ねじくぎ / nejikugi <63852665+scrwnl@users.noreply.github.com>
2025-08-02 17:18:50 +0000

    add type sockaddr_in

--- mod_socket.f90
+++ mod_socket.f90
@@ -12,28 +12,34 @@ module mod_socket
     integer(c_int), parameter :: SOCK_STREAM = 1
     integer(c_int), parameter :: SOCK_DGRAM  = 2
 
+    type, bind(c) :: sockaddr_in
+        integer(c_short) :: sin_family, sin_port
+        integer(c_int) :: sin_addr
+        character(c_char) :: sin_zero(8)
+    end type sockaddr_in
+
     interface
         ! int socket(int domain, int type, int protocol);
         function c_socket(domain, type, protocol) bind(c, name="socket")
             use, intrinsic :: iso_c_binding
-            integer(kind=c_int) :: c_socket
-            integer(kind=c_int), value :: domain, type, protocol
+            integer(c_int) :: c_socket
+            integer(c_int), value :: domain, type, protocol
         end function
     
         ! int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
         function c_bind(sockfd, addr, addrlen) bind(c, name="bind")
             use, intrinsic :: iso_c_binding
-            integer(kind=c_int) :: c_bind
-            integer(kind=c_int), value :: sockfd
+            integer(c_int) :: c_bind
+            integer(c_int), value :: sockfd
             type(c_ptr), value :: addr
-            integer(kind=c_int64_t), value :: addrlen
+            integer(c_int64_t), value :: addrlen
         end function
     
         ! int listen(int sockfd, int backlog);
         function c_listen(sockfd, backlog) bind(c, name="listen")
             use, intrinsic :: iso_c_binding
-            integer(kind=c_int) :: c_listen
-            integer(kind=c_int), value :: sockfd, backlog
+            integer(c_int) :: c_listen
+            integer(c_int), value :: sockfd, backlog
         end function
     
         ! int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
@@ -42,45 +48,45 @@ module mod_socket
             integer(c_int) :: c_connect
             integer(c_int), value :: sockfd
             type(c_ptr), value :: addr
-            integer(kind=c_int64_t), value :: addrlen
+            integer(c_int64_t), value :: addrlen
         end function
 
         ! ssize_t send(int sockfd, const void *buf, size_t len, int flags);
         function c_send(sockfd, buf, len, flags) bind(c, name="send")
             use, intrinsic :: iso_c_binding
-            integer(kind=c_int) :: c_send
-            integer(kind=c_int), value :: sockfd, len, flags
-            character(1,kind=c_char), value :: buf
+            integer(c_int) :: c_send
+            integer(c_int), value :: sockfd, len, flags
+            character(c_char), value :: buf
         end function
 
         ! ssize_t recv(int sockfd, const void *buf, size_t len, int flags);
         function c_recv(sockfd, buf, len, flags) bind(c, name="recv")
             use, intrinsic :: iso_c_binding
-            integer(kind=c_int) :: c_recv
-            integer(kind=c_int), value :: sockfd, len, flags
+            integer(c_int) :: c_recv
+            integer(c_int), value :: sockfd, len, flags
             type(c_ptr), value :: buf
         end function
 
         ! int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
         function c_accept(sockfd, addr, addrlen) bind(c, name="accept")
             use, intrinsic :: iso_c_binding
-            integer(kind=c_int) :: c_accept
-            integer(kind=c_int), value :: sockfd
+            integer(c_int) :: c_accept
+            integer(c_int), value :: sockfd
             type(c_ptr), value :: addr, addrlen
         end function
 
         ! uint32_t htonl(uint32_t hostlong);
         function c_htonl(hostlong) bind(c, name="htonl")
             use, intrinsic :: iso_c_binding
-            integer(kind=c_int) :: c_htonl
-            integer(kind=c_int), value :: hostlong
+            integer(c_int) :: c_htonl
+            integer(c_int), value :: hostlong
         end function
 
         ! uint16_t htons(uint16_t hostshort);
         function c_htons(hostshort) bind(c, name="htons")
             use, intrinsic :: iso_c_binding
-            integer(kind=c_short) :: c_htons
-            integer(kind=c_int), value :: hostshort
+            integer(c_short) :: c_htons
+            integer(c_int), value :: hostshort
         end function
 
         ! void *memset(void *s, int c, size_t n);
